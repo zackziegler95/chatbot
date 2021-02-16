9de0d362913939c8728f37a21e2641e1894a1828
Zachary Ziegler <zackziegler95@gmail.com>
2021-02-16 12:33:33 -0500

    changed wireprotocol back to good version

--- wireprotocol.py
+++ wireprotocol.py
@@ -1,5 +1,3 @@
-
-
 # version 0
 # 1 byte, version
 # 1 byte, command
@@ -8,18 +6,17 @@
 ## 1 - list accounts
 ### data: (optional) ascii text wildcard
 ## 2 - send message
-### data: ascii recipient username DELIM ascii message
+### data: ascii sender username DELIM ascii recipient username DELIM ascii message
 ## 3 - deliver undelivered messages
 ### data: none
 ## 4 - delete account
 ### data: none
 ## 5 - login
 ### data: ascii username
-## 6 - server response to login/create acct
-### data: ascii True/False (for success/failure of login or new acct action)
-## 7 - list of usernames
-### data: ascii usernames, separated by DELIM
-
+## 6 - server response
+### data: (optional) error message
+## 7 - list response
+### data: DELIM separated usernames
 # 40 bytes, data length
 # data length bytes, data
 
@@ -27,7 +24,6 @@ WP_VERSION = 0 # current version is 0
 VERSION_LEN = 1
 COMMAND_LEN = 1
 DATALENGTH_LEN = 40
-DELIM = b'|||'
 
 class CMD:
     CREATE = 0
@@ -37,20 +33,21 @@ class CMD:
     DELETE = 4
     LOGIN = 5
     RESPONSE = 6
-    ACCTS = 7
+    LISTRESPONSE = 7
+    DELIM = b'|||'
 
 class WireProtocol:
     # The idea here is for the operations of the wire protocol to be independent
     # of the details of the socket
     # This class is responsible for validating data
 
-    def __init__(self, uuid):
+    def __init__(self):
         self.reset_buffers()
 
     def reset_buffers(self):
         self.version = -1 # -1 will indicate we don't have it yet
         self.command = -1
-        self.data_len = -1
+        self.data_len = -1 
         self.data_buffer = b'' # empty byte string will indicate we don't have it yet
         self.tmp_buffer = b''
 
@@ -87,7 +84,7 @@ class WireProtocol:
         if not self.data_buffer and self.data_len > 0:
             if len(self.tmp_buffer) < self.data_len:
                 return False
-            self.data_buffer = int.from_bytes(self.tmp_buffer[:self.data_len], "big")
+            self.data_buffer = self.tmp_buffer[:self.data_len]
             self.tmp_buffer = self.tmp_buffer[self.data_len:]
 
         return True
@@ -109,9 +106,9 @@ class WireProtocol:
 
         # send message
         if self.command == CMD.SEND:
-            if DELIM not in text:
+            if CMD.DELIM not in self.data_buffer:
                 raise ValueError('data delimeter not found in message body')
-            text = [arg.decode('ascii') for arg in self.data_buffer.split(DELIM)]
+            text = [arg.decode('ascii') for arg in self.data_buffer.split(CMD.DELIM)]
             return text
 
         # deliver undelivered messages
@@ -125,58 +122,55 @@ class WireProtocol:
             if self.data_buffer:
                 raise ValueError('no data expected for delete account command')
             return None
-
+         
         # login
         if self.command == CMD.LOGIN:
             return self.data_buffer.decode('ascii')
 
-        raise ValueError('command id %d unknown' % self.command)
+        # response
+        if self.command == CMD.RESPONSE:
+            if self.data_buffer:
+                return self.data_buffer.decode('ascii')
+            else:
+                return None
 
+        # list response
+        if self.command == CMD.LISTRESPONSE:
+            text = [arg.decode('ascii') for arg in self.data_buffer.split(CMD.DELIM)]
+            return text
+
+        raise ValueError('command id %d unknown' % self.command)
+    
     @staticmethod
     def data_to_bytes(command, *args):
-        # any args passed are eventually concatenated with the DELIM delimeter
+        # any args passed are eventually concatenated with the CMD.DELIM delimeter
         # args should be a list of (normal) strings
         message = b''
 
         # version
-        if WP_VERSION >= 2**VERSION_LEN:
+        if WP_VERSION >= 2**(VERSION_LEN*8):
             raise ValueError('version %d is larger than max version %d' % (WP_VERSION, 2**VERSION_LEN-1))
         message += int.to_bytes(WP_VERSION, VERSION_LEN, 'big')
 
         # command
-        if command >= 2*COMMAND_LEN:
-            raise ValueError('command %d is larger than max version %d' % (command, 2**COMMAND_LEN-1))
+        if command >= 2**(COMMAND_LEN*8):
+            raise ValueError('command %d is larger than max command %d' % (command, 2**COMMAND_LEN-1))
         message += int.to_bytes(command, COMMAND_LEN, 'big')
 
         # data length
         if args:
             num_args = len(args)
             args = [arg.encode('ascii') for arg in args]
-            data_len = sum(map(len, args))+len(DELIM)*(num_args-1)
+            data_len = sum(map(len, args))+len(CMD.DELIM)*(num_args-1)
         else:
             data_len = 0
 
-        if data_len >= 2*DATALENGTH_LEN:
+        if data_len >= 2**(DATALENGTH_LEN*8):
             raise ValueError('data length %d is larger than max data length %d' % (data_len, 2**DATALENGTH_LEN-1))
         message += int.to_bytes(data_len, DATALENGTH_LEN, 'big')
 
         # data
         if data_len > 0:
-            message += DELIM.join(args)
+            message += CMD.DELIM.join(args)
 
         return message
-
-
-class BlockingClientSocket:
-    def __init__(self, socket):
-        self.socket = socket
-        self.wire_protocol = WireProtocol()
-
-    def send(self, msg):
-        # msg is arbitrary json
-        # loops until msg is fully sent
-        # self.wire_protocol.add_to_buffer(data)
-
-    def recv(self):
-        # msg is arbitrary json
-        # loops until msg is fully received
